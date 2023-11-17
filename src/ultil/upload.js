257713c2ced6fe22a86ba5cd466f5423a142d164
hovien2201 <124018097+hovien2201@users.noreply.github.com>
2023-11-17 10:46:20 +0700

    up

--- src/ultil/upload.js
+++ src/ultil/upload.js
@@ -4,7 +4,7 @@ const path = require('path')
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         // Thư mục đích để lưu trữ tệp hình ảnh
-        cb(null, '/codegym/newnodejs/Nodejs/src/public/image');
+        cb(null, '/Nodejs/src/public/image');
     },
     filename: function (req, file,  cb) {
         console.log(file)
